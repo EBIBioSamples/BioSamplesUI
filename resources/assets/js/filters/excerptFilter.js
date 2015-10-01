5b6b7fb9087e1e3dd9d0bac159b37311dd18fdc5
Kerruba <luca.cherubin@gmail.com>
2015-10-01 11:45:18 +0100

    Update the excerpt filter to take into account the possibility of a undefined or null value

--- resources/assets/js/filters/excerptFilter.js
+++ resources/assets/js/filters/excerptFilter.js
@@ -1,9 +1,11 @@
 module.exports = function(value,maxLength) {
-	if (typeof maxLength === "undefined" || maxLength === null) {
-		maxLength = 300;
+	if (typeof value !== "undefined" && value !== null) {
+		if (typeof maxLength === "undefined" || maxLength === null) {
+			maxLength = 300;
+		}
+		if (value.length > maxLength) {
+			return value.slice(0,maxLength) + "...";
+	 	}	
 	}
-	if (value.length > maxLength) {
-		return value.slice(0,maxLength) + "...";
- 	}
 	return value;
 };
